373a1127630baedf3055b343959e0c549534761c
norenma <markus.a.noren@gmail.com>
2013-09-24 22:37:07 +0200

    Started implementing a bluetooth-package in the android project.

---
+++ Tendu-android/src/it/chalmers/tendu/network/BluetoothClient.java
@@ -0,0 +1,50 @@
+package it.chalmers.tendu.network;
+
+import it.chalmers.tendu.gamemodel.MiniGame;
+
+import java.util.List;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class BluetoothClient implements IClient {
+	
+	public static final int REQUEST_ENABLE_BT = 666;
+	
+	private Context context;
+	private BluetoothAdapter mBluetoothAdapter;
+	
+	public BluetoothClient(Context context){
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		this.context=context;
+	}
+
+	@Override
+	public void sendObject(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void joinGame(MiniGame miniGame) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<IServer> searchTeam() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void enableBluetooth() {
+		Intent enableBtIntent; 
+		if (!mBluetoothAdapter.isEnabled()) {
+			enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+			((AndroidApplication) context).startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT); // context is wrong
+		} 
+	}
+
+}
---
+++ Tendu-android/src/it/chalmers/tendu/network/BluetoothServer.java
@@ -0,0 +1,11 @@
+package it.chalmers.tendu.network;
+
+public class BluetoothServer implements IServer {
+
+	@Override
+	public void hostGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
