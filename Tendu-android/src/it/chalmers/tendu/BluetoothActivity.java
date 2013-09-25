c1200ae87521a70f9196bcf68707f8c0d9afe05c
John Petersson <johnpetersson@gmail.com>
2013-09-25 14:25:26 +0200

    bluetooth testing

---
+++ Tendu-android/src/it/chalmers/tendu/BluetoothActivity.java
@@ -0,0 +1,37 @@
+package it.chalmers.tendu;
+
+import it.chalmers.tendu.network.INetworkHandler;
+import it.chalmers.tendu.network.bluetooth.BluetoothHandler;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class BluetoothActivity extends Activity {
+
+	INetworkHandler bluetoothHandler = new BluetoothHandler(this);
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bluetooth);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.bluetooth, menu);
+		return true;
+	}
+
+	public void onHostButtonClicked(View v) {
+		bluetoothHandler.hostSession();
+	}
+	
+	public void onJoinButtonClicked(View v) {
+		bluetoothHandler.joinGame();
+	}
+
+}
