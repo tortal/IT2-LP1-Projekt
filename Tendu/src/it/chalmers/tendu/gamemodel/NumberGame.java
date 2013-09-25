60e86375483f8524560a308b5ccca9a7953b50c1
Johanna Hartman <Johannah40@hotmail.com>
2013-09-25 14:04:56 +0200

    Logic for NumberGame

--- Tendu/src/it/chalmers/tendu/gamemodel/NumberGame.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/NumberGame.java
@@ -1,13 +1,21 @@
 package it.chalmers.tendu.gamemodel;
 
+import java.util.ArrayList;
+
 import it.chalmers.tendu.defaults.Defaults;
 import it.chalmers.tendu.defaults.GameIds;
 
 public class NumberGame extends MiniGame {
 	
+	private ArrayList<Integer> numberCode;
+	private ArrayList<Integer> randomNumbers;
+	
+	
 	public NumberGame(int addTime, int difficulty) {
 		super(addTime, difficulty, GameIds.NUMBER_GAME);
 		
+		numberCode = new ArrayList<Integer>(); 
+		randomNumbers = new ArrayList<Integer>();
 
 		switch (difficulty) {
 		case Defaults.DIFFICULTY_ONE:
@@ -16,6 +24,29 @@ public class NumberGame extends MiniGame {
 		case Defaults.DIFFICULTY_TWO:
 			this.addTime(20);
 			break;
+		}
+		
+		//Fill numberCode with random numbers
+		for(int i = 0; i < 5; i++){
+			numberCode.add(checkRandomNumber(numberCode));
+		}	
+		//Fill randomNumbers with random numbers
+		for(int i = 0; i < 5; i++){
+			randomNumbers.add(checkRandomNumber(numberCode));
+		}
+		
+	}
+	
+	public boolean checkNumber(int num, int pos){
+		return numberCode.get(pos) == num; 
+	}
+	
+	private int checkRandomNumber(ArrayList list){
+		int randomNum = 1 + (int)(Math.random()*100);
+		if(list.contains(randomNum)){
+			return checkRandomNumber(list);
+		}else{
+			return randomNum; 
 		}
 	}
 
