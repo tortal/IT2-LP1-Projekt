4ec16ee81e388caaced5a2ea6f1239e5738b4a67
luddesgit <luddel85@gmail.com>
2013-09-26 14:15:35 +0200

    refactor numbergame

--- Tendu/src/it/chalmers/tendu/gamemodel/NumberGame.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/NumberGame.java
@@ -9,50 +9,59 @@ import com.badlogic.gdx.Gdx;
 
 public class NumberGame extends MiniGame {
 
-	private ArrayList<Integer> numberCode;
-	private ArrayList<Integer> randomNumbers;
-	
-	
+	private ArrayList<Integer> answerList;
+	private ArrayList<Integer> answerAndDummyList;
+
 	public NumberGame(int addTime, Difficulty difficulty) {
 		super(addTime, difficulty, GameIds.NUMBER_GAME);
-		numberCode = new ArrayList<Integer>(); 
-		randomNumbers = new ArrayList<Integer>();
 
 		switch (difficulty) {
 		case ONE:
-			this.addTime(35);
+			this.addTime(30);
+			answerList = createAnswer(4);
 			break;
 		case TWO:
-			this.addTime(20);
+			this.addTime(30);
+			answerList = createAnswer(8);
 			break;
 		default:
 			// TODO:
 			Gdx.app.debug("NumberGame Class", "Fix this switch case");
 		}
 
-		//Fill numberCode with random numbers
-		for(int i = 0; i < 5; i++){
-			numberCode.add(checkRandomNumber(numberCode));
-		}	
-		//Fill randomNumbers with numberCode and random numbers
-		for(int i = 0; i < 5; i++){
-			randomNumbers.add(checkRandomNumber(numberCode));
-			randomNumbers.add(numberCode.get(i));
-		}
-		
-	}
-	
-	public boolean checkNumber(int num, int pos){
-		return numberCode.get(pos) == num; 
-	}
-	
-	private int checkRandomNumber(ArrayList list){
-		int randomNum = 1 + (int)(Math.random()*100);
-		if(list.contains(randomNum)){
-			return checkRandomNumber(list);
-		}else{
-			return randomNum; 
-		}
+		// Fill numberCode with random numbers
+//		for (int i = 0; i < 5; i++) {
+//			numberCode.add(checkRandomNumber(numberCode));
+//		}
+		// // Fill randomNumbers with numberCode and random numbers
+		// for (int i = 0; i < 5; i++) {
+		// randomNumbers.add(checkRandomNumber(numberCode));
+		// randomNumbers.add(numberCode.get(i));
+		// }
 	}
 
+	public boolean checkNbr(int num) {
+		
+//		return numberCode.get(pos) == num;
+		return true;
+	}
+
+	private ArrayList createAnswer(int length) {
+		ArrayList<Integer> answerList = new ArrayList<>();
+		int i=0;
+		while (i<length){
+		int randomNbr = 1 + (int)(Math.random()*100);
+			if(!(answerList.contains(randomNbr))){
+				answerList.add(randomNbr);
+				i++;
+			}
+		}
+		return answerList;
+	}
+
+	private ArrayList fillWithDummyNbr(ArrayList<Integer> list) {
+		ArrayList newList = new ArrayList();
+		// TODO populate list with original number and dummy numbers.
+		return newList;
+	}
 }
