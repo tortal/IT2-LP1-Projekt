2a696dd8b7409e93aa0e440678a63268477628e3
cmeijner <christian.meijner@gmail.com>
2013-09-27 12:08:05 +0200

    In progress

--- Tendu/src/it/chalmers/tendu/screens/NumberGameScreen.java
+++ Tendu/src/it/chalmers/tendu/screens/NumberGameScreen.java
@@ -3,6 +3,7 @@ package it.chalmers.tendu.screens;
 //TODO needs refactoring
 import it.chalmers.tendu.Tendu;
 import it.chalmers.tendu.controllers.InputController;
+import it.chalmers.tendu.defaults.GameState;
 import it.chalmers.tendu.gamemodel.MiniGame;
 import it.chalmers.tendu.gamemodel.NumberGame;
 
@@ -122,22 +123,24 @@ public class NumberGameScreen extends GameScreen {
 		shapeRenderer.setProjectionMatrix(game.getCamera().combined);
 		shapeRenderer.begin(ShapeType.Circle);
 		
-		if(time < 240) {
-			numberFont.setColor(Color.BLUE);
-			numberFont.draw(spriteBatch, "Memorize the numbers", 200, 400);
-			drawNumbers(true);
-			
-		} else {
-			numberFont.setColor(Color.BLUE);
-			numberFont.draw(spriteBatch, "Enter the numbers in the correct order", 60, 400);
-			
-			drawNumbers(false);
-
-			numberFont.scale(-0.8f);
-			for(int i = 0; i < numberCircles.size(); i++) {
-				drawNumberCircle(numberCircles.get(i));
+		if(model.checkGameState() == GameState.IN_PROGRESS) {
+			if(time < 240) {
+				numberFont.setColor(Color.BLUE);
+				numberFont.draw(spriteBatch, "Memorize the numbers", 200, 400);
+				drawNumbers(true);
+				
+			} else {
+				numberFont.setColor(Color.BLUE);
+				numberFont.draw(spriteBatch, "Enter the numbers in the correct order", 60, 400);
+				
+				drawNumbers(false);
+	
+				numberFont.scale(-0.8f);
+				for(int i = 0; i < numberCircles.size(); i++) {
+					drawNumberCircle(numberCircles.get(i));
+				}
+				numberFont.scale(0.8f);
 			}
-			numberFont.scale(0.8f);
 		}
 		
 		shapeRenderer.end();
@@ -148,43 +151,45 @@ public class NumberGameScreen extends GameScreen {
 	/** All game logic goes here */
 	@Override
 	public void tick(InputController input) {
-		if(time < 240) {
-			time++;
-		} else {
-	        if (input.isTouchedUp()) {
-				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-	            game.getCamera().unproject(touchPos);
-	            
-	            for(NumberCircle circle: numberCircles) {            	
-	            	if(touchPos.x > circle.leftX && touchPos.x < circle.rightX) {
-	                	if (touchPos.y < circle.topY && touchPos.y > circle.bottomY) {
-	                    	Gdx.input.vibrate(25);
-	                    	if(model.checkNbr(circle.getNumber())) {
-	                    		Gdx.app.log("Correct number = ", "" + circle.getNumber());
-	                    		for(Number num: numbers) {
-	                    			if(num.number == circle.getNumber()) {
-	                    				num.show = true;
-	                    			}
-	                    		}
-	                    	}
-	                	}
-	            	}      	
-	            	circle.scale=1;
-	            }
-	        } 
-	        
-	        if (Gdx.input.isTouched()) {
-				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-	            game.getCamera().unproject(touchPos);
-	            
-	            for(NumberCircle circle: numberCircles) {            	
-	            	if(touchPos.x > circle.leftX && touchPos.x < circle.rightX) {
-	                	if (touchPos.y < circle.topY && touchPos.y > circle.bottomY) {
-	                		circle.scale = 1.5f;
-	                	}
-	            	}
-	            }
-	        }
+		if(model.checkGameState() == GameState.IN_PROGRESS) {
+			if(time < 240) {
+				time++;
+			} else {
+		        if (input.isTouchedUp()) {
+					touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+		            game.getCamera().unproject(touchPos);
+		            
+		            for(NumberCircle circle: numberCircles) {            	
+		            	if(touchPos.x > circle.leftX && touchPos.x < circle.rightX) {
+		                	if (touchPos.y < circle.topY && touchPos.y > circle.bottomY) {
+		                    	Gdx.input.vibrate(25);
+		                    	if(model.checkNbr(circle.getNumber())) {
+		                    		Gdx.app.log("Correct number = ", "" + circle.getNumber());
+		                    		for(Number num: numbers) {
+		                    			if(num.number == circle.getNumber()) {
+		                    				num.show = true;
+		                    			}
+		                    		}
+		                    	}
+		                	}
+		            	}      	
+		            	circle.scale=1;
+		            }
+		        } 
+		        
+		        if (Gdx.input.isTouched()) {
+					touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+		            game.getCamera().unproject(touchPos);
+		            
+		            for(NumberCircle circle: numberCircles) {            	
+		            	if(touchPos.x > circle.leftX && touchPos.x < circle.rightX) {
+		                	if (touchPos.y < circle.topY && touchPos.y > circle.bottomY) {
+		                		circle.scale = 1.5f;
+		                	}
+		            	}
+		            }
+		        }
+			}
 		}
   
    	}
