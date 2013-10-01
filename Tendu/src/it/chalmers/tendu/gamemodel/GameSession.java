8622c1fd2806ae1eeb00c1c916102cf4bf092d85
tortal <nard@student.chalmers.se>
2013-10-01 14:20:27 +0200

    Changed locations and refactored model

--- Tendu/src/it/chalmers/tendu/gamemodel/GameSession.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/GameSession.java
@@ -3,12 +3,15 @@ package it.chalmers.tendu.gamemodel;
 
 
 import java.util.List;
+import java.util.Map;
 
 public class GameSession {
 
 	private final GameRoundSettings gameSessionSettings;
 	private MiniGame currentMinigame;
 	private List<Player> players;
+	private Map<Player, Integer> playerNbr;
+	
 
 	public GameSession(GameRoundSettings gameRoundSettings) {
 		this.gameSessionSettings = gameRoundSettings;
