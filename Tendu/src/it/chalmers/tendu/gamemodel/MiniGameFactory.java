39dbdca4de1ba86c9e8bdf7501d53e834ffaaadd
luddesgit <luddel85@gmail.com>
2013-10-02 15:28:58 +0200

    new factory

--- Tendu/src/it/chalmers/tendu/gamemodel/MiniGameFactory.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/MiniGameFactory.java
@@ -1,7 +1,11 @@
 package it.chalmers.tendu.gamemodel;
 
+import java.util.Collections;
+import java.util.List;
+
 import it.chalmers.tendu.defaults.Constants.Difficulty;
 import it.chalmers.tendu.gamemodel.numbergame.NumberGame;
+import it.chalmers.tendu.gamemodel.shapesgame.ShapesGame;
 
 /*TODO This class shall select a random minigame with the selected difficulty
  *It shall also keep track of previously played games to avoid playing the
@@ -10,21 +14,18 @@ import it.chalmers.tendu.gamemodel.numbergame.NumberGame;
 public class MiniGameFactory {
 
 	public static MiniGame createMiniGame(int bonusTime, Difficulty difficulty) {
-		MiniGame miniGame = null;
-		switch (difficulty) {
-		case ONE:
-			miniGame = new NumberGame(bonusTime, Difficulty.ONE);
-		case TWO:
-//			miniGame = new ShapesGame(addTime, Difficulty.TWO);
-			miniGame = new NumberGame(bonusTime, Difficulty.TWO);
-		case THREE:
-			break;
-		case FOUR:
-			break;
-		case FIVE:
-			break;
-		}
 		
+		MiniGame miniGame = null;
+		List<GameId> games = GameId.getGameIdsFor(difficulty);
+		Collections.shuffle(games);
+		GameId game = games.get(0);
+		
+		switch (game) {
+		case NUMBER_GAME:
+			miniGame = new NumberGame(bonusTime, difficulty);
+		case SHAPES_GAME:
+			miniGame = new ShapesGame(bonusTime, difficulty);
+		}
 		return miniGame;
 
 	}
