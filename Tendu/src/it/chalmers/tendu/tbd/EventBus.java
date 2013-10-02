27821fd5f8a1df2f731951a86c76afd62f88a5a9
tortal <nard@student.chalmers.se>
2013-10-02 11:41:16 +0200

    EventBus!

--- Tendu/src/it/chalmers/tendu/tbd/EventBus.java
+++ Tendu/src/it/chalmers/tendu/tbd/EventBus.java
@@ -1,5 +1,28 @@
 package it.chalmers.tendu.tbd;
 
-public class EventBus {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.badlogic.gdx.Gdx;
+
+public enum EventBus {
+	
+	INSTANCE;
+
+	private List<Listener> listeners;
+
+	public void broadcast(Message message){
+		Gdx.app.log("EventBus", "broadcasting" + message);
+		for (Listener l : listeners){
+			l.onBroadcast(message);
+		}
+	}
+	
+	public void addListener(Listener l) {
+		listeners.add(l);
+	}
+	
+	public void removeListener(Listener l){
+		listeners.remove(l);
+	}
 }
--- Tendu/src/it/chalmers/tendu/tbd/Message.java
+++ Tendu/src/it/chalmers/tendu/tbd/Message.java
@@ -4,4 +4,15 @@ public class Message {
 	public String tag;
 	public String msg;
 	public Object content;
+	
+	public Message(String tag, String msg, Object content){
+		this.tag = tag;
+		this.msg = msg;
+		this.content = content;
+	}
+	
+	public Message(String tag, String msg){
+		this(tag, msg, null);
+	}
+	
 }
