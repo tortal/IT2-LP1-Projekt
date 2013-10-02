9b6a06eb920ff0b86a59c3185e4ee6d29db240f1
tortal <nard@student.chalmers.se>
2013-10-02 15:08:05 +0200

    GameID update

--- Tendu/src/it/chalmers/tendu/gamemodel/GameId.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/GameId.java
@@ -1,5 +1,21 @@
 package it.chalmers.tendu.gamemodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum GameId {
-	NUMBER_GAME, SHAPES_GAME;
+	NUMBER_GAME(0,1,2,3), SHAPES_GAME(2,3,4,5,6);
+
+	
+	private List<Integer> acceptedLevels = new ArrayList<Integer>();
+	
+	
+	private GameId(Integer... integers){
+		for (Integer i : integers){
+			acceptedLevels.add(i);
+		}
+
+	}
+	
+	
 }
