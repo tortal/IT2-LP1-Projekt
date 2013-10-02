e946330e0af3b27648ff707c15f0bb350995adbd
cmeijner <christian.meijner@gmail.com>
2013-10-02 11:10:10 +0200

    EventBus + Message Classes

---
+++ Tendu/src/it/chalmers/tendu/tbd/EventBus.java
@@ -0,0 +1,5 @@
+package it.chalmers.tendu.tbd;
+
+public class EventBus {
+
+}
---
+++ Tendu/src/it/chalmers/tendu/tbd/Message.java
@@ -0,0 +1,7 @@
+package it.chalmers.tendu.tbd;
+
+public class Message {
+	public String tag;
+	public String msg;
+	public Object content;
+}
