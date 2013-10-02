afa16aa60930e5e211e34b40e1abe0c610a7c362
tortal <nard@student.chalmers.se>
2013-10-02 15:17:29 +0200

    GameID supports difficulty

--- Tendu/src/it/chalmers/tendu/gamemodel/GameId.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/GameId.java
@@ -1,26 +1,35 @@
 package it.chalmers.tendu.gamemodel;
 
+import it.chalmers.tendu.defaults.Constants.Difficulty;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public enum GameId {
-	NUMBER_GAME(0,1,2,3), SHAPES_GAME(2,3,4,5,6);
+	
+	NUMBER_GAME(Difficulty.ONE), SHAPES_GAME(Difficulty.TWO);
 
+	/**
+	 * Levels that 
+	 */
+	private List<Difficulty> acceptedDifficulties = new ArrayList<Difficulty>();
 	
-	private List<Integer> acceptedLevels = new ArrayList<Integer>();
 	
-	
-	private GameId(Integer... integers){
-		for (Integer i : integers){
-			acceptedLevels.add(i);
+	private GameId(Difficulty... difficulty){
+		for (Difficulty d : difficulty){
+			acceptedDifficulties.add(d);
 		}
 
 	}
 	
-	public static List<GameId> getAllGameIDFor(int level){
+	/**
+	 * @param level
+	 * @return a list of GameIds that accepts the given level value
+	 */
+	public static List<GameId> getAllGameIDFor(Difficulty difficulty){
 		List<GameId> validGames = new ArrayList<GameId>();
 		for (GameId g : values()){
-			if (g.acceptedLevels.contains(level))
+			if (g.acceptedDifficulties.contains(difficulty))
 				validGames.add(g);
 		}
 		return validGames;
