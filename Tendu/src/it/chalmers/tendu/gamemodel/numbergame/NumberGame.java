e1d2f9ea0361e9f3bc645538fa2f76fde6edd6d8
luddesgit <luddel85@gmail.com>
2013-10-03 16:33:28 +0200

    hax

--- Tendu/src/it/chalmers/tendu/gamemodel/numbergame/NumberGame.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/numbergame/NumberGame.java
@@ -143,7 +143,8 @@ public class NumberGame extends MiniGame {
 	 * @return
 	 */
 	public ArrayList<Integer> getPlayerList(int player) {
-		return playerLists.get(player);
+//		return playerLists.get(player);
+		return answerList;
 	}
 
 	/**
