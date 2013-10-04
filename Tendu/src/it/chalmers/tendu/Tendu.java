22f83e92b9d27c5d6c4a2911898f78d67ad636e3
cmeijner <christian.meijner@gmail.com>
2013-10-04 17:20:38 +0200

    starting lobby as client too

--- Tendu/src/it/chalmers/tendu/Tendu.java
+++ Tendu/src/it/chalmers/tendu/Tendu.java
@@ -186,6 +186,8 @@ public class Tendu implements ApplicationListener, Listener {
 			break;
 		case LOAD_THIS_GAME:
 			if(!host) {
+				gameSession = gameLobby.getGameSession();
+				modelController = new ModelController(this, gameSession);
 				Gdx.app.log(TAG, "LOAD_THIS_GAME");
 				gameSession.setCurrentMiniGame((MiniGame)message.content);
 				setScreen(MiniGameScreenFactory.createMiniGameScreen(this, (MiniGame)message.content));
