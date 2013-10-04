875dd3676af76716a0fe87d1990819dabc53da14
John Petersson <johnpetersson@gmail.com>
2013-10-04 11:14:00 +0200

    Added no args constructor in EventMessage

--- Tendu/src/it/chalmers/tendu/tbd/EventMessage.java
+++ Tendu/src/it/chalmers/tendu/tbd/EventMessage.java
@@ -5,11 +5,15 @@ public class EventMessage {
 	public C.Msg msg;
 	public Object content;
 	
+	/** No args constructor for reflection */
+	EventMessage() {
+	}
+	
 	public EventMessage(C.Tag tag, C.Msg msg, Object content){
 		this.tag = tag;
 		this.msg = msg;
 		this.content = content;
-	}
+	}	
 	
 	public EventMessage(C.Tag tag, C.Msg msg){
 		this(tag, msg, null);
