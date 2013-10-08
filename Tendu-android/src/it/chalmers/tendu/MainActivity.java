7201cb0a2eb66ad9dd4dd5d7521afb66e611d9de
John Petersson <johnpetersson@gmail.com>
2013-10-08 14:04:03 +0200

    MainActivity starting WifiHAndler

--- Tendu-android/src/it/chalmers/tendu/MainActivity.java
+++ Tendu-android/src/it/chalmers/tendu/MainActivity.java
@@ -2,6 +2,7 @@ package it.chalmers.tendu;
 
 import it.chalmers.tendu.network.INetworkHandler;
 import it.chalmers.tendu.network.bluetooth.BluetoothHandler;
+import it.chalmers.tendu.network.wifip2p.WifiHandler;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -16,7 +17,7 @@ public class MainActivity extends AndroidApplication {
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useGL20 = false;
         
-        INetworkHandler netHandler = new BluetoothHandler(this);
+        INetworkHandler netHandler = new WifiHandler(this);
         initialize(new Tendu(netHandler), cfg);
     }
 }
