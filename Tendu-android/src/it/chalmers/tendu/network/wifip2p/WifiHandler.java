efbde6b91da80b11b0a3f617a64ddaa52ac53568
John Petersson <johnpetersson@gmail.com>
2013-10-09 17:30:17 +0200

    Revert "logging disconnects"
    This reverts commit a3c603a2515b4326e6376eb714e9dcbef0f87351.

--- Tendu-android/src/it/chalmers/tendu/network/wifip2p/WifiHandler.java
+++ Tendu-android/src/it/chalmers/tendu/network/wifip2p/WifiHandler.java
@@ -110,7 +110,7 @@ public class WifiHandler extends NetworkHandler implements WifiP2pManager.Connec
 	@Override
 	public void hostSession() {
 		// remove any remaining wifi group
-		//	removeWifiGroup();
+			removeWifiGroup();
 			
 		// Create a new wifi group
 //		mManager.createGroup(mChannel, new WifiP2pManager.ActionListener() {
@@ -139,7 +139,7 @@ public class WifiHandler extends NetworkHandler implements WifiP2pManager.Connec
 
 	@Override
 	public void joinGame() {
-		//removeWifiGroup();
+		removeWifiGroup();
 		discoverPeers();
 		mHandler.postDelayed(new Runnable() {
 
@@ -366,7 +366,6 @@ public class WifiHandler extends NetworkHandler implements WifiP2pManager.Connec
 			}
 			@Override
 			public void disconnected(Connection connection) {
-				Log.d(TAG, "Client disconnected");
 				server.close();
 			}
 		});
@@ -398,7 +397,6 @@ public class WifiHandler extends NetworkHandler implements WifiP2pManager.Connec
 				}
 				@Override
 				public void disconnected(Connection connection) {
-					Log.d(TAG, "Disconnected from server");
 					client.close();
 				}
 			});
