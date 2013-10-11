1529eea4ca0a296990e7a7e7f0a9c111be4a1f6e
tortal <nard@student.chalmers.se>
2013-10-11 10:49:10 +0200

    New branch. added javadoc

--- Tendu/src/it/chalmers/tendu/tbd/Listener.java
+++ Tendu/src/it/chalmers/tendu/tbd/Listener.java
@@ -2,6 +2,10 @@ package it.chalmers.tendu.tbd;
 
 public interface Listener {
 
+	/**
+	 * This method is called when a call is made to {@link EventBus} 
+	 * @param message that was broadcasted.
+	 */
 	public void onBroadcast(EventMessage message);
 
 }
