c99273ffbcefa64ee78b47bc918a096ef57a66e5
LuddesGit <luddel85@gmail.com>
2013-10-14 20:42:38 +0200

    GameOver Bitch!

---
+++ Tendu/src/it/chalmers/tendu/screens/GameOverScreen.java
@@ -0,0 +1,25 @@
+package it.chalmers.tendu.screens;
+
+import it.chalmers.tendu.controllers.InputController;
+
+public class GameOverScreen implements Screen {
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tick(InputController input) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removed() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
