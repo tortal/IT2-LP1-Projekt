e1bf45c33d5f46a4f209a8e66e0d4652a520e94a
tortal <nard@student.chalmers.se>
2013-10-14 21:25:33 +0200

    EventBus. weak hash map, with synchronized wrapper

--- Tendu/src/it/chalmers/tendu/tbd/EventBus.java
+++ Tendu/src/it/chalmers/tendu/tbd/EventBus.java
@@ -1,7 +1,10 @@
 package it.chalmers.tendu.tbd;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 import com.badlogic.gdx.Gdx;
 
@@ -9,27 +12,67 @@ public enum EventBus {
 
 	INSTANCE;
 
-	private List<Listener> listeners = new ArrayList<Listener>();
+	public final static String TAG = "EventBus";
+
+	private List<Listener> liners;
+	private Map<Listener, Void> listeners;
+
+	EventBus() {
+		WeakHashMap<Listener, Void> l = new WeakHashMap<Listener, Void>();
+		listeners = Collections.synchronizedMap(l);
+	}
 
 	public void broadcast(EventMessage message) {
-		Gdx.app.log("EventBus", "broadcasting" + message);
-		synchronized (listeners) {
-			// for (Listener l : listeners){
-			// l.onBroadcast(message);
-			// }
+		synchronized (this) {
+			Gdx.app.log(TAG, "broadcasting" + message);
 
-			for (int i = 0; i < listeners.size(); i++) {
-				listeners.get(i).onBroadcast(message);
+			Iterator<Listener> iter = listeners.keySet().iterator();
+			while (iter.hasNext()) {
+				iter.next().onBroadcast(message);
 			}
 
 		}
 	}
 
 	public void addListener(Listener l) {
-		listeners.add(l);
+		synchronized (this) {
+			listeners.put(l, null);
+		}
 	}
 
 	public void removeListener(Listener l) {
-		listeners.remove(l);
+		synchronized (this) {
+			listeners.remove(l);
+		}
+
 	}
 }
+
+// public enum EventBus {
+//
+// INSTANCE;
+//
+// private List<Listener> listeners = new ArrayList<Listener>();
+//
+// public void broadcast(EventMessage message) {
+// Gdx.app.log("EventBus", "broadcasting" + message);
+// synchronized (listeners) {
+// // for (Listener l : listeners){
+// // l.onBroadcast(message);
+// // }
+//
+// for (int i = 0; i < listeners.size(); i++) {
+// listeners.get(i).onBroadcast(message);
+// }
+//
+// }
+// }
+//
+// public void addListener(Listener l) {
+// listeners.add(l);
+// }
+//
+// public void removeListener(Listener l) {
+// listeners.remove(l);
+// }
+// }
