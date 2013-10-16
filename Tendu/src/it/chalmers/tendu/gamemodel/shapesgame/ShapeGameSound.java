e13954eb62478b3ff0919c57b681ba321adcf291
Johanna Hartman <Johannah40@hotmail.com>
2013-10-16 18:39:03 +0200

    fail sound

--- Tendu/src/it/chalmers/tendu/gamemodel/shapesgame/ShapeGameSound.java
+++ Tendu/src/it/chalmers/tendu/gamemodel/shapesgame/ShapeGameSound.java
@@ -29,7 +29,7 @@ public class ShapeGameSound implements Listener {
 				.internal("completed.wav"));
 		lostGameSound = Gdx.audio.newSound(Gdx.files.internal("gamelost.wav"));
 		succeededSound = Gdx.audio.newSound(Gdx.files.internal("success.wav"));
-		failSound = Gdx.audio.newSound(Gdx.files.internal("fail.aiff"));
+		failSound = Gdx.audio.newSound(Gdx.files.internal("fail.wav"));
 	}
 
 	@Override
