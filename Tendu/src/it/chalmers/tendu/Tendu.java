8e88b05ac9ef46c01ca46e0b6992d4dfb09728a9
LuddesGit <luddel85@gmail.com>
2013-10-16 13:06:12 +0200

    reset network when going back to menu screen

--- Tendu/src/it/chalmers/tendu/Tendu.java
+++ Tendu/src/it/chalmers/tendu/Tendu.java
@@ -167,6 +167,7 @@ public class Tendu implements ApplicationListener, Listener {
 			SessionResult sessionResult = (SessionResult)message.content;
 			Screen screen = new GameOverScreen(this, sessionResult);
 			setScreen(screen);
+			networkHandler.resetNetwork();
 			
 		} else if (message.msg == C.Msg.RESTART){
 			// TODO: Unregister network
