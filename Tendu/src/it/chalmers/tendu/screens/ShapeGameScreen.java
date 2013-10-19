6373876925f8991c697f155f1e36895a2c3485ec
norenma <markus.a.noren@gmail.com>
2013-10-19 15:26:15 +0200

    small fixes

--- Tendu/src/it/chalmers/tendu/screens/ShapeGameScreen.java
+++ Tendu/src/it/chalmers/tendu/screens/ShapeGameScreen.java
@@ -27,6 +27,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class ShapeGameScreen extends GameScreen {
 
 	public final String TAG = this.getClass().getName();
+	private final int SEND_MARGIN = 110;
 
 	private int player_num;
 	private ShapeRenderer shapeRenderer; // used to render vector graphics
@@ -98,7 +99,6 @@ public class ShapeGameScreen extends GameScreen {
 							.getLockSequence().size() + 1);
 		}
 
-
 		otherPlayers = controller.getModel().getOtherPlayerNumbers();
 
 	}
@@ -107,7 +107,8 @@ public class ShapeGameScreen extends GameScreen {
 	@Override
 	public void render() {
 		super.render();
-		if (shapeGameModel.checkGameState() == GameState.RUNNING) {
+		if (shapeGameModel.checkGameState() == GameState.RUNNING ||
+				gameCompletedTimer.isRunning()) {
 			shapeRenderer.setProjectionMatrix(tendu.getCamera().combined);
 			// Renders locks
 			for (GraphicalShape sgs : locks) {
@@ -128,22 +129,30 @@ public class ShapeGameScreen extends GameScreen {
 	 */
 	private void sendToTeamMate(GraphicalShape s) {
 		Gdx.app.log(TAG, "SHAPE SENDING!!!!!!!!");
-		if (s.getBounds().y >= Constants.SCREEN_HEIGHT - 160
+		if (s.getBounds().y >= Constants.SCREEN_HEIGHT - SEND_MARGIN
 				&& otherPlayers.size() >= 1) {
-			EventBus.INSTANCE.broadcast(new EventMessage(/*Player.getInstance()
-					.getMac(), */C.Tag.TO_SELF, C.Msg.SHAPE_SENT, controller
-					.getModel().getGameId(), messageContentFactory(controller
-					.getModel().getOtherPlayerNumbers().get(0), s.getShape())));
-		} else if (s.getBounds().x <= 160 && otherPlayers.size() >= 2) {
-			EventBus.INSTANCE.broadcast(new EventMessage(/*Player.getInstance()
-					.getMac(),*/ C.Tag.TO_SELF, C.Msg.SHAPE_SENT, controller
-					.getModel().getGameId(), messageContentFactory(otherPlayers.get(1), s.getShape())));
-		}
-		else if (s.getBounds().x >= Constants.SCREEN_WIDTH - 160
+			EventBus.INSTANCE.broadcast(new EventMessage(/*
+														 * Player.getInstance()
+														 * .getMac(),
+														 */C.Tag.TO_SELF,
+					C.Msg.SHAPE_SENT, controller.getModel().getGameId(),
+					messageContentFactory(controller.getModel()
+							.getOtherPlayerNumbers().get(0), s.getShape())));
+		} else if (s.getBounds().x <= SEND_MARGIN && otherPlayers.size() >= 2) {
+			EventBus.INSTANCE.broadcast(new EventMessage(/*
+														 * Player.getInstance()
+														 * .getMac(),
+														 */C.Tag.TO_SELF,
+					C.Msg.SHAPE_SENT, controller.getModel().getGameId(),
+					messageContentFactory(otherPlayers.get(1), s.getShape())));
+		} else if (s.getBounds().x >= Constants.SCREEN_WIDTH - SEND_MARGIN
 				&& otherPlayers.size() >= 3) {
-			EventBus.INSTANCE.broadcast(new EventMessage(/*Player.getInstance()
-					.getMac(), */C.Tag.TO_SELF, C.Msg.SHAPE_SENT, controller
-					.getModel().getGameId(), messageContentFactory(otherPlayers.get(2), s.getShape())));
+			EventBus.INSTANCE.broadcast(new EventMessage(/*
+														 * Player.getInstance()
+														 * .getMac(),
+														 */C.Tag.TO_SELF,
+					C.Msg.SHAPE_SENT, controller.getModel().getGameId(),
+					messageContentFactory(otherPlayers.get(2), s.getShape())));
 		}
 	}
 
@@ -157,14 +166,14 @@ public class ShapeGameScreen extends GameScreen {
 	 */
 	private NetworkShape messageContentFactory(int player, Shape shape) {
 
-//		List<Object> l = new ArrayList<Object>();
-//		l.add(player);
-//		l.add(shape);
-//		return l;
-		
-		//return Player.getInstance();
+		// List<Object> l = new ArrayList<Object>();
+		// l.add(player);
+		// l.add(shape);
+		// return l;
+
+		// return Player.getInstance();
 		return new NetworkShape(player, shape);
-//		return 1;
+		// return 1;
 	}
 
 	/** All game logic goes here (within the model...) */
@@ -179,7 +188,7 @@ public class ShapeGameScreen extends GameScreen {
 			EventBus.INSTANCE.broadcast(message);
 		} else if (!gameCompletedTimer.isRunning()) {
 			if (controller.getModel().checkGameState() == GameState.WON) {
-				gameCompletedTimer.start(1500);
+				gameCompletedTimer.start(750);
 				controller.getModel().stopTimer();
 				Gdx.app.log(TAG, "Timer started! game won");
 
@@ -188,8 +197,6 @@ public class ShapeGameScreen extends GameScreen {
 			}
 
 		}
-		
-		
 
 		// TODO nullpointer movingShape
 		if (input.isTouchedDown()) {
@@ -241,7 +248,7 @@ public class ShapeGameScreen extends GameScreen {
 					latestAddedShape = latestModelReceivedShape;
 				}
 			}
-			
+
 		}
 
 		// Removes shapes that are no longer part of the model
@@ -265,29 +272,6 @@ public class ShapeGameScreen extends GameScreen {
 			for (GraphicalShape gs : removeList)
 				shapes.remove(gs);
 		}
-		
-		// Adds shapes to the gui that are no longer part
-		// of the model.
-		// for (Shape s : shapeGameModel.getAllInventory().get(player_num)) {
-		// if (!shapes.contains(new GraphicalShape(s))) {
-		// shapes.add(new GraphicalShape(s));
-		// Gdx.app.log(TAG, "new Shape!");
-		// }
-		// }
-
-		// Removes shapes that are no longer parts of the
-		// model.
-		// List<GraphicalShape> removeList = new ArrayList<GraphicalShape>();
-		// for (GraphicalShape gs : shapes) {
-		// if (!shapeGameModel.getAllInventory().get(player_num)
-		// .contains(gs.getShape())) {
-		// removeList.add(gs);
-		// Gdx.app.log(TAG, "Shape removed!");
-		// }
-		// }
-		//
-		// for (GraphicalShape gs : removeList)
-		// shapes.remove(gs);
 
 	}
 
@@ -297,7 +281,6 @@ public class ShapeGameScreen extends GameScreen {
 			if (shapeGameModel.shapeFitIntoLock(player_num, shape.getShape(),
 					lock.getShape())) {
 				shape.moveShape(lock.getBounds().x, lock.getBounds().y);
-				// shape.getShape().setLocked(true);
 				result = true;
 				Gdx.app.log(TAG, "Animated" + "x=" + lock.getBounds().x + "y="
 						+ lock.getBounds().getY());
@@ -309,9 +292,12 @@ public class ShapeGameScreen extends GameScreen {
 			content.add(shape.getShape());
 
 			// Received by ShapeGameController.
-			EventBus.INSTANCE.broadcast(new EventMessage(/*Player.getInstance()
-					.getMac(), */C.Tag.TO_SELF, C.Msg.LOCK_ATTEMPT, controller
-					.getModel().getGameId(), content));
+			EventBus.INSTANCE.broadcast(new EventMessage(/*
+														 * Player.getInstance()
+														 * .getMac(),
+														 */C.Tag.TO_SELF,
+					C.Msg.LOCK_ATTEMPT, controller.getModel().getGameId(),
+					content));
 		}
 
 		return result;
@@ -340,12 +326,6 @@ public class ShapeGameScreen extends GameScreen {
 		GraphicalShape receivedShape = new GraphicalShape(shape);
 		if (!otherPlayers.contains(sender))
 			return false;
-
-		// for (GraphicalShape s : shapes) {
-		// if (s.getShape().equals(shape))
-		// receivedShape = s;
-		// }
-		Gdx.app.log(TAG, "Shapes being added");
 
 		shapes.add(receivedShape);
 
