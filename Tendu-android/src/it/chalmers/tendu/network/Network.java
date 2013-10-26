1b81e05bc5c195cf3876bed858cd1562180742e9
John Petersson <johnpetersson@gmail.com>
2013-10-26 18:40:17 +0200

    minor

--- Tendu-android/src/it/chalmers/tendu/network/Network.java
+++ Tendu-android/src/it/chalmers/tendu/network/Network.java
@@ -36,7 +36,7 @@ public class Network implements INetwork {
 	
 	@Override
 	public boolean isWifip2pAvailable() {
-		return (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN);
+		return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN);
 	}
 	
 	@Override
